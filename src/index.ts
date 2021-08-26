2b0a59c65e12929ec561b2ee51b49614f45f601d
matheusifz <matheusifz@gmail.com>
2021-08-26 19:22:18 -0300

    feat: integrate mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import { json } from "body-parser";
 import { addressRouter } from "./routes/address";
-
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
 app.use(addressRouter);
+
+mongoose
+  .connect(
+    "mongodb+srv://matheusifz:sabrina07@cluster0.iezls.mongodb.net/CEP?retryWrites=true&w=majority",
+    {}
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 app.listen(3000, () => {
   console.log("Server is Listening on port 3000");
